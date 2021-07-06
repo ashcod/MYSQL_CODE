95a0a8335a69f1a9ed627c43bac3323ea7bde43d
ashcod <64306882+ashcod@users.noreply.github.com>
2021-07-06 16:10:00 +0530

    Update all_possible_combinations.sql

--- all_possible_combinations.sql
+++ all_possible_combinations.sql
@@ -1,4 +1,28 @@
+--input --
+
+-- Teamname
++------------+
+-- INDIA
+-- AUSTRALIA
+-- ENGLAND
+-- NEWZELAND
+
+-- output --
+
+-- matches
++--------------------+
+-- AUSTRALIA - INDIA
+-- AUSTRALIA - ENGLAND
+-- AUSTRALIA - NEWZELAND
+-- ENGLAND - INDIA
+-- ENGLAND - NEWZELAND
+-- INDIA - NEWZELAND
+
+
 select DISTINCT(CONCAT(a.Teamname,' - ',b.Teamname)) as matches
 from teams a, teams b
 where a.Teamname < b.Teamname
 order by a.Teamname
+
+
+Note: These are all possible combinations, meaning combination of AUSTRALIA - INDIA is same as INDIA - AUSTRALIA so it wont appear second time.
