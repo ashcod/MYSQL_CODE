51e742e04d553d6042cddf135e09961235bbec30
ashcod <64306882+ashcod@users.noreply.github.com>
2021-07-16 21:10:41 +0530

    Add files via upload

---
+++ create_database_exercise.sql
@@ -0,0 +1,7 @@
+-- Create the “Sales” database. To do this, you’ll have to use one of the two possibilities shown in the lecture, and then execute the code!
+
+-- Creating a database called Sales
+CREATE DATABASE IF NOT EXISTS Sales;
+
+-- Creating a Sales_new database [Alternative to create database]
+CREATE SCHEMA IF NOT EXISTS Sales;
