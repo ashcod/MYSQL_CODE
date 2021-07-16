4c909dc71bef874039a1c5b4284b7a419993dbb7
ashcod <64306882+ashcod@users.noreply.github.com>
2021-07-16 21:12:35 +0530

    Update create_database_exercise.sql

--- create_database_exercise.sql
+++ create_database_exercise.sql
@@ -1,6 +1,8 @@
 -- Create the “Sales” database. To do this, you’ll have to use one of the two possibilities shown in the lecture, and then execute the code!
 
 -- Creating a database called Sales
+-- IF NOT EXISTS will check if the database is already exists or not
+
 CREATE DATABASE IF NOT EXISTS Sales;
 
 -- Creating a Sales_new database [Alternative to create database]
